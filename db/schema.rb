9344b64770250790a385e6be5fdafc8d403497f9
Railsbridge Student <unconfigured-student@railsbridge.local>
2016-06-25 16:13:20 +0000

    Allow People To Vote

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,8 @@ ActiveRecord::Schema.define(version: 20160625145240) do
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "votes", force: :cascade do |t|
